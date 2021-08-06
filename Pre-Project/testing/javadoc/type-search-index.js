755d68c9eba8258542aa349cdea5cf647e91954d
Jade Fjestad <40924189+TheJade@users.noreply.github.com>
2021-08-06 12:44:39 -0600

    Final commit this is done

---
+++ Pre-Project/testing/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"B1Listener"},{"p":"<Unnamed>","l":"B2Listener"},{"p":"<Unnamed>","l":"B4Listener"},{"p":"<Unnamed>","l":"Data"},{"p":"<Unnamed>","l":"GUI"},{"p":"<Unnamed>","l":"MainLoop"},{"p":"<Unnamed>","l":"Reader"}];updateSearchResults();
--- Pre-Project/testing/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"MainLoop","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"MainLoop","l":"MainLoop()","u":"%3Cinit%3E()"}];updateSearchResults();
